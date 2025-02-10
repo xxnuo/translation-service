2fb56bb614a00da79ed7b19d4fb1dd7ba5dcfa5d
Kerry <54252779+xxnuo@users.noreply.github.com>
2025-02-10 16:21:24 +0800

    Update env.h

--- src/env.h
+++ src/env.h
@@ -24,18 +24,22 @@ inline void loadEnvFile(const std::string& path = ".env") {
         }
 
         std::string key = line.substr(0, pos);
-        std::string value = line.substr(pos + 1);
+        std::string value = pos + 1 < line.length() ? line.substr(pos + 1) : "";
 
         // 去除首尾空格
         key.erase(0, key.find_first_not_of(" \t"));
         key.erase(key.find_last_not_of(" \t") + 1);
-        value.erase(0, value.find_first_not_of(" \t"));
-        value.erase(value.find_last_not_of(" \t\r") + 1);
+        
+        // 只有当值不为空时才进行值的处理
+        if (!value.empty()) {
+            value.erase(0, value.find_first_not_of(" \t"));
+            value.erase(value.find_last_not_of(" \t\r") + 1);
 
-        // 如果值被引号包围，去除引号
-        if (value.size() >= 2 && (value[0] == '"' || value[0] == '\'') && 
-            value[0] == value[value.size()-1]) {
-            value = value.substr(1, value.size() - 2);
+            // 如果值被引号包围，去除引号
+            if (value.size() >= 2 && (value[0] == '"' || value[0] == '\'') && 
+                value[0] == value[value.size()-1]) {
+                value = value.substr(1, value.size() - 2);
+            }
         }
 
         // 只在环境变量不存在时设置
@@ -47,5 +51,8 @@ inline void loadEnvFile(const std::string& path = ".env") {
 
 inline std::string getEnvVar(const std::string& key, const std::string& defaultValue) {
     const char* val = getenv(key.c_str());
-    return val ? std::string(val) : defaultValue;
+    if (!val || val[0] == '\0') {  // 如果环境变量不存在或为空字符串
+        return defaultValue;
+    }
+    return std::string(val);
 }
